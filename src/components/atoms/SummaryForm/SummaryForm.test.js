4b558284636733bb7ae3a5143137dea0d1910cc7
nahhuaraujo <nahhuaraujo@gmail.com>
2022-07-28 18:20:27 -0300

    Add: Test for Terms popover

--- src/components/atoms/SummaryForm/SummaryForm.test.js
+++ src/components/atoms/SummaryForm/SummaryForm.test.js
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { SummaryForm } from '../../atoms';
 
@@ -15,15 +16,32 @@ describe('SummaryForm', () => {
 
   test('Checkbox disables button on first click and enables it in the second click', () => {
     render(<SummaryForm />);
+
     const termsConfirmationCheckbox = screen.getByRole('checkbox', { name: /terms and conditions/i });
     const confirmOrderButton = screen.getByRole('button', { name: /confirm order/i });
 
-    fireEvent.click(termsConfirmationCheckbox);
+    userEvent.click(termsConfirmationCheckbox);
     expect(termsConfirmationCheckbox).toBeChecked();
     expect(confirmOrderButton).toBeEnabled();
 
-    fireEvent.click(termsConfirmationCheckbox);
+    userEvent.click(termsConfirmationCheckbox);
     expect(termsConfirmationCheckbox).not.toBeChecked();
     expect(confirmOrderButton).toBeDisabled();
+  });
+
+  test('Terms and Conditions hover/unhover triggers popover', () => {
+    render(<SummaryForm />);
+
+    const nullTermsPopover = screen.queryByText(/I won't recieve any icecream/i);
+    expect(nullTermsPopover).not.toBeInTheDocument();
+
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditions);
+
+    const termsPopover = screen.getByText(/I won't recieve any icecream/i);
+    expect(termsPopover).toBeInTheDocument();
+
+    userEvent.unhover(termsAndConditions);
+    expect(termsPopover).not.toBeInTheDocument();
   });
 });
