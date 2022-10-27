9c8076aefa5fe44032b59da9142ff843528b3d75
Nahuel Araujo <nahhuaraujo@gmail.com>
2022-10-27 09:41:17 -0300

    Add default border for AlertBanner

--- src/components/AlertBanner/AlertBanner.styled.ts
+++ src/components/AlertBanner/AlertBanner.styled.ts
@@ -4,15 +4,19 @@ interface Props {
   variant?: string;
 }
 
-const border = css`
+const variantBorder = css`
   border: 1px solid ${(props: Props) => props.variant};
+`;
+
+const defaultBorder = css`
+  border: 1px solid red;
 `;
 
 export const AlertBanner = styled.div`
   padding: 0.3rem 1rem;
   margin: 1rem;
   width: max-content;
-  background-color: ${(props: Props) => props.variant};
-  ${(props: Props) => (props.variant ? border : 'border-color: 1px solid red')}
+  background-color: ${(props: Props) => (props.variant ? props.variant : 'red')};
+  ${(props: Props) => (props.variant ? variantBorder : defaultBorder)}
   border-radius: 10px;
 `;
