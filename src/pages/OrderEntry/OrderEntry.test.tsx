8599bb9bdeb7fb68494907fc6c625482c82739b4
Nahuel Araujo <nahhuaraujo@gmail.com>
2022-11-03 18:16:23 -0300

    Add test for summary total

--- src/pages/OrderEntry/OrderEntry.test.tsx
+++ src/pages/OrderEntry/OrderEntry.test.tsx
@@ -1,7 +1,10 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { rest } from 'msw';
+import { Provider } from 'react-redux';
 import { OrderEntry } from '.';
 import { server } from '../../mocks/server';
+import store from '../../redux/store';
 
 describe('OrderEntry', () => {
   test('Shows alert banner when service responds with error', async () => {
@@ -14,11 +17,43 @@ describe('OrderEntry', () => {
       })
     );
 
-    render(<OrderEntry />);
+    render(
+      <Provider store={store}>
+        <OrderEntry />
+      </Provider>
+    );
 
     await waitFor(async () => {
       const alerts = await screen.findAllByRole('alert');
       expect(alerts).toHaveLength(2);
     });
+  });
+
+  test('Updates total when scoops or toppings change', async () => {
+    const user = userEvent.setup();
+    render(
+      <Provider store={store}>
+        <OrderEntry />
+      </Provider>
+    );
+
+    const chocolateScoop = await screen.findByRole('spinbutton', { name: 'Chocolate' });
+    await user.clear(chocolateScoop);
+    await user.type(chocolateScoop, '2');
+
+    const total = screen.getByText('Grand total', { exact: false });
+    expect(total).toHaveTextContent('4');
+
+    const mmsTopping = await screen.findByRole('checkbox', { name: 'M&Ms' });
+    await user.click(mmsTopping);
+    expect(mmsTopping).toBeChecked();
+
+    expect(total).toHaveTextContent('5.5');
+
+    await user.clear(chocolateScoop);
+    await user.type(chocolateScoop, '0');
+    await user.click(mmsTopping);
+
+    expect(total).toHaveTextContent('0');
   });
 });
