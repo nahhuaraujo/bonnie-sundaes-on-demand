5273f161287e036f68a3b397d2208ed33092ac6e
Nahuel Araujo <nahhuaraujo@gmail.com>
2022-11-03 17:06:36 -0300

    Add summary action for ToppingOption and fix label

--- src/components/ToppingOption/ToppingOption.tsx
+++ src/components/ToppingOption/ToppingOption.tsx
@@ -1,4 +1,5 @@
 import * as S from './ToppingOption.styled';
+import { useSummaryActions } from '../../hooks';
 
 interface Props {
   name: string;
@@ -7,12 +8,18 @@ interface Props {
 }
 
 const ToppingOption = (props: Props) => {
+  const { changeTopping } = useSummaryActions();
+
+  const checkHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeTopping({ toppingName: props.name });
+  };
+
   return (
     <S.ToppingOption>
       <img alt={`${props.name} topping`} title={`${props.name} topping`} src={`http://localhost:3030/${props.imagePath}`} width={props.imageWidth} />
       <div>
-        <input id='topping' type='checkbox' />
-        <label htmlFor='topping'>{props.name}</label>
+        <input id={`${props.name}-topping`} type='checkbox' onChange={checkHandler} />
+        <label htmlFor={`${props.name}-topping`}>{props.name}</label>
       </div>
     </S.ToppingOption>
   );
